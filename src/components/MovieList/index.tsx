bf29aad81b1d3fed42ee800cf552fafe95fec1c0
Limber Jhonathan <limberjhonathan@gmail.com>
2024-11-26 21:39:37 -0300

    movie list

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,29 +1,45 @@
 "use client";
+import React from 'react';
 import axios from "axios"
 import './index.scss'
 import { useEffect, useState } from "react"
 import MovieCard from "../MovieCard";
 import { Movie } from "@/types/movie";
+// import ReactLoading from 'react-loading';
+import { OrbitProgress } from 'react-loading-indicators';
 
 export default function MovieList(){
     const [movies, setMovies] = useState<Movie[]>([])
-
+    const [isLoading, setIsLoading] = useState<boolean>(true)
     useEffect(() => {
         getMovies()
     }, [])
 
-    const getMovies = () => {
-        axios({
+    const getMovies = async () => {
+        await axios({
             method: 'GET',
             url: 'https://api.themoviedb.org/3/discover/movie',
             params: {
                 api_key: 'cfed136a7046b3428282a74772c85eb8',
-                language: 'pt-BR'
+                language: 'pt-BR',
+                include_adult: false
             }
         }).then(response => {
             setMovies(response.data.results)
             console.log(response.data.results)
         })
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }
+
+    if(isLoading) {
+        return(
+            <div className="loading-container">
+                <OrbitProgress color="#bea422" size="medium" text="" textColor="" />
+            </div>
+        )
     }
 
     return(
