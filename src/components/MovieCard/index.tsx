11bc1395fcd2e36e6bd663861c685f329cd926ea
Limber Jhonathan <limberjhonathan@gmail.com>
2024-11-26 21:45:22 -0300

    config eslintrc

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -12,7 +12,6 @@ export default function MovieCard(props: Props){
         <li className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
-
             </div>
             <div className="movie-infos">
                 <p className="movie-title">{movie.title}</p>
