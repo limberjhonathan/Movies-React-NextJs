a1e91b05f8c22964c172d089af302a7f69243274
Limber Jhonathan <limberjhonathan@gmail.com>
2024-12-03 23:57:46 -0300

    type movie details

---
+++ src/types/movieDiscription.ts
@@ -0,0 +1,9 @@
+export interface MovieDiscription{
+    id: number
+    title: string,
+    poster_path: string,
+    overview: string,
+    vote_average: number,
+    vote_count: number,
+    popularity: number
+}
