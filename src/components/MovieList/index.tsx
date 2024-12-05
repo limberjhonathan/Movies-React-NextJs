8caf5cd6bfc30198a870a3f53000070272b66b10
Limber Jhonathan <limberjhonathan@gmail.com>
2024-12-04 23:36:45 -0300

    ajustes

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,12 +1,11 @@
 "use client";
 import React from 'react';
-import axios from "axios"
 import './index.scss'
 import { useEffect, useState } from "react"
 import MovieCard from "../MovieCard";
 import { Movie } from "@/types/movie";
-// import ReactLoading from 'react-loading';
 import { OrbitProgress } from 'react-loading-indicators';
+import { GetList } from '@/app/services/movieServices';
 
 export default function MovieList(){
     const [movies, setMovies] = useState<Movie[]>([])
@@ -16,24 +15,14 @@ export default function MovieList(){
     }, [])
 
     const getMovies = async () => {
-        await axios({
-            method: 'GET',
-            url: 'https://api.themoviedb.org/3/discover/movie',
-            params: {
-                api_key: 'cfed136a7046b3428282a74772c85eb8',
-                language: 'pt-BR',
-                include_adult: false
-            }
-        }).then(response => {
-            setMovies(response.data.results)
-            console.log(response.data.results)
-        })
-
+        const moviesList = await GetList();
+        setMovies(moviesList)
         setTimeout(() => {
             setIsLoading(false);
-        }, 2000);
+        }, 1000);
     }
 
+    console.log(movies)
     if(isLoading) {
         return(
             <div className="loading-container">
