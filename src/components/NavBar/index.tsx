a0d8371ad75266c037b07aedd6900b0019a72b80
Limber Jhonathan <limberjhonathan@gmail.com>
2024-12-12 23:14:08 -0300

    ajuste no Link

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,10 +4,10 @@ import './index.scss';
 export default function NavBar() {
     return (
         <nav className="navbar">
-            <Link href="/" className="page-title">
+            <Link href="/" className="page-title" aria-label="Página inicial - Filmes">
                 Filmes
             </Link>
-            <Link href='/movies/1' className='all-movies'>
+            <Link href='/movies/1' className='all-movies' aria-label="Ver todos os filmes">
                 Todos os filmes
             </Link>
         </nav>
