72eacaf176496f753ca810f79c8e286d43ec49f1
Limber Jhonathan <limberjhonathan@gmail.com>
2024-12-12 23:13:10 -0300

    ajuste de logica

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import './index.scss'
+import { useState } from 'react';
 
 export interface Props {
     currentPage: number | undefined
@@ -7,6 +8,8 @@ export interface Props {
 
 export default function Pagination({ currentPage }: Props) {
     const page = currentPage ?? 1;
+    const [selectedPage, setSelectedPage] = useState<number>(page);
+
     const pageNumbers: number[] = [];
     const pageInitial = page < 3 ? 1 : page - 1
     const pageMax = pageInitial == 1 ? pageInitial + 3 : pageInitial + 2
@@ -15,20 +18,23 @@ export default function Pagination({ currentPage }: Props) {
         pageNumbers.push(pageNumber);
     }
 
-    
+    const signal = (pageNum: number) => {
+        console.log(pageNum)
+        setSelectedPage(pageNum)
+    }
 
     return (
         <ul className="pagination">
-            {page == 3 && 
+            {page > 3 && 
                 <Link className="page-btn" href='/movies/1'>Primeira</Link>
             }
             {page > 1 && 
-                <a className="page-btn" href=''>Anterior</a>
+                <a className="page-btn"href={`${page-1}`}>Anterior</a>
             }
             {pageNumbers.map(page => (
-                <Link key={page} className="page-btn" href={`/movies/${page}`}>{page}</Link>
+                <Link key={page} className={`page-btn ${selectedPage === page ? 'selected' : ''}`} href={`/movies/${page}`} onClick={() => signal(page)}>{page}</Link>
             ))}
-            <a className="page-btn" href=''>Próximo</a>
+            <a className="page-btn" href={`${page+1}`}>Próximo</a>
         </ul>
     )
 }
