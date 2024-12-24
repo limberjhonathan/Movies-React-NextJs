43f8bb91eff8c695dd0b1cd416562a8e353f6d50
Limber Jhonathan <limberjhonathan@gmail.com>
2024-12-23 22:11:52 -0300

    rota para o maxPage

--- src/app/movies/[page]/page.tsx
+++ src/app/movies/[page]/page.tsx
@@ -7,6 +7,7 @@ import { OrbitProgress } from 'react-loading-indicators';
 import { GetList } from '@/app/services/movieServices';
 import MovieCard from '@/components/MovieCard';
 import Pagination from '@/components/Pagination';
+import { useRouter } from 'next/navigation';
 
 interface ErrorResponse {
     status_message?: string;
@@ -17,11 +18,13 @@ interface Props {
 }
 
 export default function MovieList({ params }: Props) {
+    const router = useRouter();
+
     const [movies, setMovies] = useState<Movie[] | undefined>([])
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [page, setPage] = useState<number>(1);
     const [nextPage, setNextPage] = useState<boolean>(true)
-    const [lastPage, setLastPage] = useState<number>(0);
+    // const [lastPage, setLastPage] = useState<number>(0);
 
     useEffect(() => {
         const getMovies = async () => {
@@ -29,35 +32,34 @@ export default function MovieList({ params }: Props) {
             const pageNumber = parseInt(resolveParam.page);
             setPage(pageNumber);
 
-            const moviesList = await GetList(pageNumber);
-
-            setMovies(moviesList?.success ? moviesList.data.results : []);
-
             const hasNextPage = await GetList(pageNumber + 1);
             if (!hasNextPage?.success) {
                 const error = hasNextPage?.error as ErrorResponse;
-                
+
                 const errorMessage = error?.status_message
 
                 const regex = /max at (\d+)/;
                 const match = errorMessage?.match(regex);
 
                 const maxPage = match ? parseInt(match[1]) : 0;
-                setLastPage(maxPage)
+                // setLastPage(maxPage)
+
+                if (pageNumber > maxPage) {
+                    console.log(`Redirecionando para a página ${maxPage}`);
+                    router.push(`/movies/${maxPage}`);
+                    return;
+                }
             }
+            console.log("passei por aqui")
+            const moviesList = await GetList(pageNumber);
+            setMovies(moviesList?.success ? moviesList.data.results : []);
 
             setNextPage(hasNextPage?.success ?? false);
             setIsLoading(false);
 
         };
         getMovies();
-    }, [params]);
-
-    // console.log(">>>>>>>>>>>>>>>>", movies)
-
-    if (lastPage && page > lastPage) {
-        console.log(`Redirecionado para a pagina ${lastPage}`)
-    }
+    }, [params, router]);
 
     if (isLoading) {
         return (
